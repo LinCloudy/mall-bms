34a3e75d658b9131849203b702c15375297d216b
Edy <1731193812@qq.com>
2020-08-17 20:58:21 +0800

    add form pre-validate

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,22 +4,34 @@
       <div class="avatar_box">
         <img src="./../assets/logo.png" alt="" />
       </div>
-      <el-form label-width="0px" class="form">
-        <el-form-item>
+      <el-form
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginForm"
+        label-width="0px"
+        class="form"
+      >
+        <el-form-item prop="username">
           <el-input
+            v-model="loginForm.username"
             placeholder=""
             prefix-icon="el-icon-user"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
+            v-model="loginForm.password"
             placeholder=""
             prefix-icon="el-icon-lock"
           ></el-input>
         </el-form-item>
         <el-form-item class="btns">
-          <el-button type="primary" plain>submit</el-button>
-          <el-button type="info" plain>reset</el-button>
+          <el-button type="primary" plain @click="submit('loginForm')"
+            >submit</el-button
+          >
+          <el-button type="info" plain @click="reset('loginForm')"
+            >reset</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
@@ -30,7 +42,52 @@
 export default {
   name: 'login',
   data() {
-    return {};
+    const checkPWD = (rule, value, callback) => {
+      const inputVal = value.trim();
+
+      if (inputVal === '' || inputVal === null) {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      loginRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: ['change', 'blur'],
+          },
+        ],
+        password: [
+          {
+            required: true,
+            validator: checkPWD,
+            trigger: ['change', 'blur'],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    reset(formName) {
+      this.$refs[formName].resetFields();
+    },
   },
 };
 </script>
